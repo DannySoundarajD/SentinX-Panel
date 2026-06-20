67ef4dd3e68bf28d34f2504dbee8e5f6ccb7b5b8
DannySoundarajD <soundarajdanny@gmail.com>
2026-06-20 01:16:50 +0000

    Add SentinX notification center prototype

---
+++ source/xfce4-notifyd/panel-plugin/sentinx-notification-center.h
@@ -0,0 +1,9 @@
+#ifndef SENTINX_NOTIFICATION_CENTER_H
+#define SENTINX_NOTIFICATION_CENTER_H
+
+#include <gtk/gtk.h>
+
+GtkWidget *sentinx_notification_center_create(void);
+void sentinx_notification_center_show(void);
+
+#endif

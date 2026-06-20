dce06543c73a64d1168d1764ea4a16f93b644f65
DannySoundarajD <soundarajdanny@gmail.com>
2026-06-20 01:54:41 +0000

    Prototype window version completed

--- source/xfce4-notifyd/panel-plugin/sentinx-notification-center.c
+++ source/xfce4-notifyd/panel-plugin/sentinx-notification-center.c
@@ -11,6 +11,79 @@ on_window_delete(GtkWidget *widget,
     return TRUE;
 }
 
+static GtkWidget *
+create_notification_card(
+    const gchar *icon,
+    const gchar *title_text,
+    const gchar *message_text,
+    const gchar *time_text)
+{
+    GtkWidget *frame;
+    GtkWidget *box;
+    GtkWidget *title;
+    GtkWidget *message;
+    GtkWidget *time;
+
+    gchar markup[512];
+
+    frame = gtk_frame_new(NULL);
+
+    box = gtk_box_new(
+        GTK_ORIENTATION_VERTICAL,
+        5
+    );
+
+    gtk_container_add(
+        GTK_CONTAINER(frame),
+        box
+    );
+
+    g_snprintf(
+        markup,
+        sizeof(markup),
+        "<b>%s %s</b>",
+        icon,
+        title_text
+    );
+
+    title = gtk_label_new(NULL);
+
+    gtk_label_set_markup(
+        GTK_LABEL(title),
+        markup
+    );
+
+    gtk_box_pack_start(
+        GTK_BOX(box),
+        title,
+        FALSE,
+        FALSE,
+        5
+    );
+
+    message = gtk_label_new(message_text);
+
+    gtk_box_pack_start(
+        GTK_BOX(box),
+        message,
+        FALSE,
+        FALSE,
+        5
+    );
+
+    time = gtk_label_new(time_text);
+
+    gtk_box_pack_start(
+        GTK_BOX(box),
+        time,
+        FALSE,
+        FALSE,
+        5
+    );
+
+    return frame;
+}
+
 GtkWidget *sentinx_notification_center_create(void)
 {
     GtkWidget *main_box;
@@ -28,14 +101,7 @@ GtkWidget *sentinx_notification_center_create(void)
     GtkWidget *btn_dnd;
     GtkWidget *btn_clear;
 
-    GtkWidget *card1;
-    GtkWidget *card2;
-
-    GtkWidget *card1_box;
-    GtkWidget *card2_box;
-
-    GtkWidget *title;
-    GtkWidget *desc;
+    GtkWidget *card;
 
     GtkWidget *date_label;
     GtkWidget *calendar;
@@ -62,21 +128,29 @@ GtkWidget *sentinx_notification_center_create(void)
         650
     );
 
-    main_box =
-        gtk_box_new(
-            GTK_ORIENTATION_VERTICAL,
-            0
-        );
+    gtk_window_set_position(
+        GTK_WINDOW(window),
+        GTK_WIN_POS_CENTER
+    );
+
+    gtk_window_set_resizable(
+        GTK_WINDOW(window),
+        TRUE
+    );
+
+    main_box = gtk_box_new(
+        GTK_ORIENTATION_VERTICAL,
+        0
+    );
 
     gtk_container_add(
         GTK_CONTAINER(window),
         main_box
     );
 
-    paned =
-        gtk_paned_new(
-            GTK_ORIENTATION_HORIZONTAL
-        );
+    paned = gtk_paned_new(
+        GTK_ORIENTATION_HORIZONTAL
+    );
 
     gtk_box_pack_start(
         GTK_BOX(main_box),
@@ -86,17 +160,15 @@ GtkWidget *sentinx_notification_center_create(void)
         0
     );
 
-    left_panel =
-        gtk_box_new(
-            GTK_ORIENTATION_VERTICAL,
-            10
-        );
+    left_panel = gtk_box_new(
+        GTK_ORIENTATION_VERTICAL,
+        10
+    );
 
-    right_panel =
-        gtk_box_new(
-            GTK_ORIENTATION_VERTICAL,
-            10
-        );
+    right_panel = gtk_box_new(
+        GTK_ORIENTATION_VERTICAL,
+        10
+    );
 
     gtk_paned_pack1(
         GTK_PANED(paned),
@@ -127,16 +199,12 @@ GtkWidget *sentinx_notification_center_create(void)
         10
     );
 
-    scroll =
-        gtk_scrolled_window_new(
-            NULL,
-            NULL
-        );
-
-    gtk_widget_set_vexpand(
-        scroll,
-        TRUE
+    scroll = gtk_scrolled_window_new(
+        NULL,
+        NULL
     );
+
+    gtk_widget_set_vexpand(scroll, TRUE);
 
     gtk_box_pack_start(
         GTK_BOX(left_panel),
@@ -146,116 +214,65 @@ GtkWidget *sentinx_notification_center_create(void)
         0
     );
 
-    notif_box =
-        gtk_box_new(
-            GTK_ORIENTATION_VERTICAL,
-            10
-        );
+    notif_box = gtk_box_new(
+        GTK_ORIENTATION_VERTICAL,
+        10
+    );
 
     gtk_container_add(
         GTK_CONTAINER(scroll),
         notif_box
     );
 
-    card1 = gtk_frame_new(NULL);
+    card = create_notification_card(
+        "🌐",
+        "WiFi Connected",
+        "Connected to SentinX-5G",
+        "2m ago"
+    );
 
     gtk_box_pack_start(
         GTK_BOX(notif_box),
-        card1,
+        card,
         FALSE,
         FALSE,
         5
     );
 
-    card1_box =
-        gtk_box_new(
-            GTK_ORIENTATION_VERTICAL,
-            5
-        );
-
-    gtk_container_add(
-        GTK_CONTAINER(card1),
-        card1_box
+    card = create_notification_card(
+        "🤖",
+        "Ollama Started",
+        "Qwen Model Loaded",
+        "5m ago"
     );
-
-    title =
-        gtk_label_new(
-            "🌐 WiFi Connected"
-        );
-
-    gtk_box_pack_start(
-        GTK_BOX(card1_box),
-        title,
-        FALSE,
-        FALSE,
-        5
-    );
-
-    desc =
-        gtk_label_new(
-            "Connected to SentinX-5G"
-        );
-
-    gtk_box_pack_start(
-        GTK_BOX(card1_box),
-        desc,
-        FALSE,
-        FALSE,
-        5
-    );
-
-    card2 = gtk_frame_new(NULL);
 
     gtk_box_pack_start(
         GTK_BOX(notif_box),
-        card2,
+        card,
         FALSE,
         FALSE,
         5
     );
 
-    card2_box =
-        gtk_box_new(
-            GTK_ORIENTATION_VERTICAL,
-            5
-        );
-
-    gtk_container_add(
-        GTK_CONTAINER(card2),
-        card2_box
+    card = create_notification_card(
+        "🛡",
+        "Sentinel Active",
+        "Monitoring System",
+        "10m ago"
     );
-
-    title =
-        gtk_label_new(
-            "🤖 Ollama Started"
-        );
 
     gtk_box_pack_start(
-        GTK_BOX(card2_box),
-        title,
+        GTK_BOX(notif_box),
+        card,
         FALSE,
         FALSE,
         5
     );
 
-    desc =
-        gtk_label_new(
-            "Qwen Model Loaded"
-        );
-
-    gtk_box_pack_start(
-        GTK_BOX(card2_box),
-        desc,
-        FALSE,
-        FALSE,
-        5
+    footer = gtk_box_new(
+        GTK_ORIENTATION_HORIZONTAL,
+        10
     );
-
-    footer =
-        gtk_box_new(
-            GTK_ORIENTATION_HORIZONTAL,
-            10
-        );
 
     gtk_box_pack_end(
         GTK_BOX(left_panel),
@@ -265,10 +282,9 @@ GtkWidget *sentinx_notification_center_create(void)
         10
     );
 
-    btn_dnd =
-        gtk_button_new_with_label(
-            "DND"
-        );
+    btn_dnd = gtk_button_new_with_label(
+        "DND"
+    );
 
     gtk_box_pack_start(
         GTK_BOX(footer),
@@ -278,10 +294,9 @@ GtkWidget *sentinx_notification_center_create(void)
         5
     );
 
-    btn_clear =
-        gtk_button_new_with_label(
-            "Clear All"
-        );
+    btn_clear = gtk_button_new_with_label(
+        "Clear All"
+    );
 
     gtk_box_pack_end(
         GTK_BOX(footer),
@@ -291,10 +306,9 @@ GtkWidget *sentinx_notification_center_create(void)
         5
     );
 
-    date_label =
-        gtk_label_new(
-            "Friday\nJune 20 2026"
-        );
+    date_label = gtk_label_new(
+        "Friday\nJune 20 2026"
+    );
 
     gtk_box_pack_start(
         GTK_BOX(right_panel),
@@ -304,8 +318,7 @@ GtkWidget *sentinx_notification_center_create(void)
         10
     );
 
-    calendar =
-        gtk_calendar_new();
+    calendar = gtk_calendar_new();
 
     gtk_box_pack_start(
         GTK_BOX(right_panel),
@@ -315,15 +328,13 @@ GtkWidget *sentinx_notification_center_create(void)
         10
     );
 
-    events_frame =
-        gtk_frame_new(
-            "Today"
-        );
+    events_frame = gtk_frame_new(
+        "Today"
+    );
 
-    events_label =
-        gtk_label_new(
-            "No Events"
-        );
+    events_label = gtk_label_new(
+        "No Events"
+    );
 
     gtk_container_add(
         GTK_CONTAINER(events_frame),
